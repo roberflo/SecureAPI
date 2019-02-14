fb37afe484c17e4b1b916cc2fea143e80ae62efb
Roberto Flores <synrflores@crecer.com.sv>
2019-02-14 14:13:25 -0600

    No share hash and salt and role in DTO

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -7,9 +7,8 @@ namespace WebApi.Dtos
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; }
+       
 
-         public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
+        
     }
 }
