1f47c97b4bde0d3e86fadef9906e566e67cc70a4
Roberto Flores <synrflores@crecer.com.sv>
2019-02-14 17:24:12 -0600

    implement localDb

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -2,12 +2,12 @@ namespace WebApi.Dtos
 {
     public class UserDto
     {
-        public int Id { get; set; }
+        public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-       
+        public string Token { get; set; }
 
         
     }
